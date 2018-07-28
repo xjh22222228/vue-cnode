676f29d5ff5947c8dc6c9b7a66f2f116084c35c2
xiejiahe <mb06@qq.com>
2018-07-29 00:53:05 +0800

    Update View

---
+++ precache-manifest.1ffebd5123316dab35fcce943db0140d.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ded730434f631bdc808",
+    "url": "js/chunk-a770.201807290051.js"
+  },
+  {
+    "revision": "188cc502ca9d5202f3f91dc7c7dba2dd",
+    "url": "img/animate.gif"
+  },
+  {
+    "revision": "b85e9c195cc1e9fa0870",
+    "url": "js/chunk-255d.201807290051.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "a549b4905bbc64594df6",
+    "url": "js/chunk-2f1e.201807290051.js"
+  },
+  {
+    "revision": "6881733b6d4d424992d049e695281fdd",
+    "url": "index.html"
+  },
+  {
+    "revision": "a790b9388724599dbf57",
+    "url": "js/chunk-3420.201807290051.js"
+  },
+  {
+    "revision": "56f101be38cd79ceb38dbea3c8a29acb",
+    "url": "img/not-match.56f101be.gif"
+  },
+  {
+    "revision": "c4bbf0ae1780165fcd31",
+    "url": "js/chunk-6fb1.201807290051.js"
+  },
+  {
+    "revision": "131c9e89f8147a72dc4187bfbde0f7db",
+    "url": "img/github.131c9e89.svg"
+  },
+  {
+    "revision": "3a86b94527e5e986f54e",
+    "url": "js/chunk-70b4.201807290051.js"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "499abd30f51f0a6d5ebd",
+    "url": "js/chunk-71d9.201807290051.js"
+  },
+  {
+    "revision": "8d4344ee532249518a1c",
+    "url": "js/chunk-vendors.201807290051.js"
+  },
+  {
+    "revision": "24d8ec55f22bfdf978ee",
+    "url": "js/app.201807290051.js"
+  },
+  {
+    "revision": "7d0241517a796c4e5822",
+    "url": "js/chunk-b98d.201807290051.js"
+  },
+  {
+    "revision": "1f2d7fa98112548afac7",
+    "url": "js/chunk-ae89.201807290051.js"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "7d0241517a796c4e5822",
+    "url": "css/chunk-b98d.201807290051.css"
+  },
+  {
+    "revision": "8d4344ee532249518a1c",
+    "url": "css/chunk-vendors.201807290051.css"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "1f2d7fa98112548afac7",
+    "url": "css/chunk-ae89.201807290051.css"
+  },
+  {
+    "revision": "7ded730434f631bdc808",
+    "url": "css/chunk-a770.201807290051.css"
+  },
+  {
+    "revision": "499abd30f51f0a6d5ebd",
+    "url": "css/chunk-71d9.201807290051.css"
+  },
+  {
+    "revision": "3a86b94527e5e986f54e",
+    "url": "css/chunk-70b4.201807290051.css"
+  },
+  {
+    "revision": "c4bbf0ae1780165fcd31",
+    "url": "css/chunk-6fb1.201807290051.css"
+  },
+  {
+    "revision": "a790b9388724599dbf57",
+    "url": "css/chunk-3420.201807290051.css"
+  },
+  {
+    "revision": "a549b4905bbc64594df6",
+    "url": "css/chunk-2f1e.201807290051.css"
+  },
+  {
+    "revision": "24d8ec55f22bfdf978ee",
+    "url": "css/app.201807290051.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+
+importScripts(
+  "precache-manifest.1ffebd5123316dab35fcce943db0140d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "vue-cnode"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
