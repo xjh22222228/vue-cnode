5a8e08b256ff6b180ee803a98ec2af1a8c66237a
xiejiahe <mb06@qq.com>
2018-10-01 19:54:29 +0800

    Update View

---
+++ precache-manifest.0f80df8b1ee254d31cb3d417073be572.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cee3171e8c0315aa1be",
+    "url": "js/chunk-6f7e497b.201810011953.js"
+  },
+  {
+    "revision": "188cc502ca9d5202f3f91dc7c7dba2dd",
+    "url": "img/animate.gif"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "ff55b1e4d5173c2aaec4",
+    "url": "js/chunk-29ed73c0.201810011953.js"
+  },
+  {
+    "revision": "28fc398c7aa2edae3081143052b16591",
+    "url": "index.html"
+  },
+  {
+    "revision": "5c8cd5a45b3111ea9730",
+    "url": "js/chunk-3965a02c.201810011953.js"
+  },
+  {
+    "revision": "56f101be38cd79ceb38dbea3c8a29acb",
+    "url": "img/not-match.56f101be.gif"
+  },
+  {
+    "revision": "137538e12ba1613efedb",
+    "url": "js/chunk-3b19765e.201810011953.js"
+  },
+  {
+    "revision": "0385a9ceb236e48d0b7b",
+    "url": "js/chunk-578f5212.201810011953.js"
+  },
+  {
+    "revision": "131c9e89f8147a72dc4187bfbde0f7db",
+    "url": "img/github.131c9e89.svg"
+  },
+  {
+    "revision": "ccb300cfd2759179a1ae",
+    "url": "js/chunk-62571240.201810011953.js"
+  },
+  {
+    "revision": "a2d7adf9cdce98988904",
+    "url": "js/chunk-vendors.201810011953.js"
+  },
+  {
+    "revision": "611dd8d7a4d18bb4c21a",
+    "url": "js/app.201810011953.js"
+  },
+  {
+    "revision": "dfc9525eb0f68dbccaae",
+    "url": "js/chunk-f510f838.201810011953.js"
+  },
+  {
+    "revision": "646091d9f8e9f7e6585b",
+    "url": "js/chunk-78230433.201810011953.js"
+  },
+  {
+    "revision": "c71699dd6ca5584fbf94",
+    "url": "js/chunk-a0cdc9da.201810011953.js"
+  },
+  {
+    "revision": "dfc9525eb0f68dbccaae",
+    "url": "css/chunk-f510f838.201810011953.css"
+  },
+  {
+    "revision": "c71699dd6ca5584fbf94",
+    "url": "css/chunk-a0cdc9da.201810011953.css"
+  },
+  {
+    "revision": "646091d9f8e9f7e6585b",
+    "url": "css/chunk-78230433.201810011953.css"
+  },
+  {
+    "revision": "4cee3171e8c0315aa1be",
+    "url": "css/chunk-6f7e497b.201810011953.css"
+  },
+  {
+    "revision": "ccb300cfd2759179a1ae",
+    "url": "css/chunk-62571240.201810011953.css"
+  },
+  {
+    "revision": "137538e12ba1613efedb",
+    "url": "css/chunk-3b19765e.201810011953.css"
+  },
+  {
+    "revision": "5c8cd5a45b3111ea9730",
+    "url": "css/chunk-3965a02c.201810011953.css"
+  },
+  {
+    "revision": "ff55b1e4d5173c2aaec4",
+    "url": "css/chunk-29ed73c0.201810011953.css"
+  },
+  {
+    "revision": "611dd8d7a4d18bb4c21a",
+    "url": "css/app.201810011953.css"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.3addbcafbb84beb36f582a7afb782730.js"
+  "precache-manifest.0f80df8b1ee254d31cb3d417073be572.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-cnode"});
